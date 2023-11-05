fac81ee4b0722d18ce0bf9e678d2b165267f007b
wrlandev <109324822+wrlandev@users.noreply.github.com>
2023-11-05 18:28:29 -0300

    Base criada

---
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EmprestimoLivros.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public String Recebedor { get; set; }
+        public string Fornecedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime dataAtualizacao { get; set; } = DateTime.Now;
+    }
+}
