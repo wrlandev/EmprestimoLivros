77b61a164a0d721417a051aa224825d6e4e8522c
wrlandev <109324822+wrlandev@users.noreply.github.com>
2023-11-05 20:28:05 -0300

    CRUD criado

---
+++ EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimosController.cs
@@ -0,0 +1,99 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace EmprestimoLivros.Controllers
+{
+	public class EmprestimosController : Controller
+	{
+		readonly private ApplicationDbContext _context;
+		public EmprestimosController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			IEnumerable<EmprestimosModel> emprestimos = _context.Empretimos;
+			return View(emprestimos);
+		}
+
+		public IActionResult Cadastrar()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Cadastrar(EmprestimosModel emprestimo)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Empretimos.Add(emprestimo);
+				_context.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public IActionResult Editar(int? id)
+		{
+			if(id == null || id == 0)
+			{
+				return NotFound();
+			}
+			EmprestimosModel emprestimo = _context.Empretimos.FirstOrDefault(x => x.Id == id);
+
+			if(emprestimo == null)
+			{
+				return NotFound();
+			}
+
+			return View(emprestimo);
+		}
+
+		[HttpPost]
+		public IActionResult Editar(EmprestimosModel emprestimo)
+		{
+			if(ModelState.IsValid)
+			{
+				_context.Empretimos.Update(emprestimo);
+				_context.SaveChanges();
+
+				return RedirectToAction("Index");
+			}
+
+			return View(emprestimo);
+		}
+
+		public IActionResult Excluir(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			EmprestimosModel emprestimo = _context.Empretimos.FirstOrDefault(x => x.Id == id);
+
+			if (emprestimo == null)
+			{
+				return NotFound();
+			}
+
+			return View(emprestimo);
+		}
+
+		[HttpPost]
+		public IActionResult Excluir(EmprestimosModel emprestimo)
+		{
+			if (emprestimo == null)
+			{
+				return NotFound();
+			}
+
+			_context.Empretimos.Remove(emprestimo);
+			_context.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
+	}
+}
--- EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
@@ -1,10 +1,15 @@
-﻿namespace EmprestimoLivros.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Models
 {
     public class EmprestimosModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Recebedor!")]
         public String Recebedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Fornecedor!")]
         public string Fornecedor { get; set; }
+        [Required(ErrorMessage = "Digite o nome do Livro!")]
         public string LivroEmprestado { get; set; }
         public DateTime dataAtualizacao { get; set; } = DateTime.Now;
     }
