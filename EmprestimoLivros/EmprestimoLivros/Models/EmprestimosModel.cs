b24ecb84495362c3c606228511366e89d9dd2ebe
Wrlan.Dev <109324822+wrlandev@users.noreply.github.com>
2023-11-10 19:28:28 -0300

    Validações na Model

--- EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
@@ -5,11 +5,17 @@ namespace EmprestimoLivros.Models
     public class EmprestimosModel
     {
         public int Id { get; set; }
+
         [Required(ErrorMessage = "Digite o nome do Recebedor!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Mínimo de 3 caracteres")]
         public String Recebedor { get; set; }
+
         [Required(ErrorMessage = "Digite o nome do Fornecedor!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Mínimo de 3 caracteres")]
         public string Fornecedor { get; set; }
+
         [Required(ErrorMessage = "Digite o nome do Livro!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Mínimo de 3 caracteres")]
         public string LivroEmprestado { get; set; }
         public DateTime dataAtualizacao { get; set; }
     }
